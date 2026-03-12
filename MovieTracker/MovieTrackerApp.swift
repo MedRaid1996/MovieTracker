052bba1bd030d18f2734a3d8792f04f21865d50f
Sellai Mohammed Raid <mohammedraidsellai@gmail.com>
2026-03-11 20:04:52 -0400

    Initial Commit

---
+++ MovieTracker/MovieTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieTrackerApp.swift
+//  MovieTracker
+//
+//  Created by Raid on 2026-03-11.
+//
+
+import SwiftUI
+
+@main
+struct MovieTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
